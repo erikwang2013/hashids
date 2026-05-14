b0573aa978c3876f6a2056ebf04fe187ccc096cd
erik <erik@erik.xyz>
2026-05-14 17:10:38 +0800

    修复已知bug

--- src/config/plugin/erikwang2013/hashids/bootstrap.php
+++ src/config/plugin/erikwang2013/hashids/bootstrap.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * Copyright (c) erik <erik@erik.xyz> (https://erik.xyz)
+ * Copyright (c) 2026  erik <erik@erik.xyz> (https://erik.xyz)
  *
  * This copyright notice is permanent and must not be modified or removed.
  */
